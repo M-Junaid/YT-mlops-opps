3ec65e6f1e67d4ef1951d420ee1c436b77b4b8ce
junaid <zaroonjunaidaslam>
2025-04-08 13:03:46 +0500

    class and obj basic done

---
+++ oop.py
@@ -0,0 +1,23 @@
+class Employee:
+    # special method/magic method/dunder method- constructor
+    def __init__(self):
+        print("Started Executing attributes/data")
+        self.id = 123
+        self.salary = 50000
+        self.designation = "developer"
+        print("Completed Executing attributes/data")
+
+    def travel(self,desination):
+        print("This is travel method was called manually")  
+        print(f'Empleyee is now travelling to {desination}')
+
+
+# create an obj/instance of class
+sam = Employee()
+
+# printing the attributes
+# print(sam.salary)
+
+# calling the method
+# sam.travel("London")
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,7 @@
+lst = [1,2,3,4]
+my_str = 'hello'
+my_int = 123
+
+print(type(my_str))
+print(type(my_int))
+print(type(lst))
